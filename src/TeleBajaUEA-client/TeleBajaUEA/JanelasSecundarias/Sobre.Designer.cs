dda32fcba59a4048466b51cca2757340eab96479
Rodrigo Bernardino <rbbernardino@gmail.com>
2015-12-22 17:43:46 -0400

    Atualizando para testar se oftware instalado atualiza também automaticamente...

--- src/TeleBajaUEA-client/TeleBajaUEA/JanelasSecundarias/Sobre.Designer.cs
+++ src/TeleBajaUEA-client/TeleBajaUEA/JanelasSecundarias/Sobre.Designer.cs
@@ -39,24 +39,24 @@
             // button1
             // 
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(23, 124);
+            this.button1.Location = new System.Drawing.Point(91, 213);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(59, 32);
+            this.button1.Size = new System.Drawing.Size(59, 70);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(106, 124);
+            this.button2.Location = new System.Drawing.Point(202, 121);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 32);
+            this.button2.Size = new System.Drawing.Size(126, 82);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(268, 124);
+            this.button3.Location = new System.Drawing.Point(380, 213);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 32);
             this.button3.TabIndex = 2;
@@ -67,15 +67,15 @@
             // 
             this.labelEquipe.AutoSize = true;
             this.labelEquipe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelEquipe.Location = new System.Drawing.Point(75, 45);
+            this.labelEquipe.Location = new System.Drawing.Point(198, 21);
             this.labelEquipe.Name = "labelEquipe";
-            this.labelEquipe.Size = new System.Drawing.Size(189, 24);
+            this.labelEquipe.Size = new System.Drawing.Size(135, 24);
             this.labelEquipe.TabIndex = 3;
-            this.labelEquipe.Text = "Team Baja UEA 2015";
+            this.labelEquipe.Text = "Baja UEA 2015";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(277, 196);
+            this.button4.Location = new System.Drawing.Point(406, 298);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
@@ -87,7 +87,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 231);
+            this.ClientSize = new System.Drawing.Size(505, 333);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.labelEquipe);
             this.Controls.Add(this.button3);
@@ -95,7 +95,6 @@
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Sobre";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sobre - TeleBaja UEA";
             this.ResumeLayout(false);
             this.PerformLayout();
