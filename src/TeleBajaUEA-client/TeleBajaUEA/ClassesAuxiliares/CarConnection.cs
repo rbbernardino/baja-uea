968e88fd673c7f56210762dc9afb2b62d2258481
Rodrigo Bernardino <rbbernardino@gmail.com>
2016-01-22 10:22:14 -0400

    algumas funções transferidas do SerialPortBaja para o CarConnection (funções relativas ao XBee e protocolo do TeleBaja)

--- src/TeleBajaUEA-client/TeleBajaUEA/ClassesAuxiliares/CarConnection.cs
+++ src/TeleBajaUEA-client/TeleBajaUEA/ClassesAuxiliares/CarConnection.cs
@@ -3,6 +3,7 @@ using System.Timers;
 using System.Threading.Tasks;
 using TeleBajaUEA.ClassesAuxiliares;
 using TeleBajaUEA.RaceDataStructs;
+using System;
 
 namespace TeleBajaUEA
 {
@@ -17,11 +18,39 @@ namespace TeleBajaUEA
         private static event NewDataHandler NewDataArrived;
         private delegate void NewDataHandler(object source, SensorsData newData);
 
-        private static SerialPortBaja portXBee;
+        private static SerialPortExt portXBee;
 
         private static int UPDATE_BYTE_RATE_INTERVAL = 1000;
         private static Timer timerUpdateByteRate;
         private static uint prevReceivedBytes;
+
+        #region Protocol
+        //private static SemaphoreSlim canReceiveDataSignal;
+        private static string XB_READY = "READY"; // ENVIA: checa se XBee pode iniciar envio
+        private static string XB_START = "START"; // ENVIA: pede por inicio do envio de dados
+        private static string XB_BEGIN = "B"; // RECEBE: inicio de pacote
+        private static string XB_END = "E"; // RECEBE: fim de pacote
+        #endregion
+
+        #region XBee settings
+        private static int XBEE_CMD_DELAY = 100; // valor do param GT do XBee
+        private static char XBEE_LINE_END = '\r';
+        private static string XBEE_CAR_ID = "XBEE_CARRO";
+        #endregion
+
+        #region Temporary Sensor Measurements
+        // variáveis temporárias dos dados de um pacote que está sendo recebido
+        private static uint tmpMillis; // tempo da medicao em milisegundos
+        private static float tmpSpeed;
+        private static float tmpTemperature;
+        private static float tmpRpm;
+        private static char tmpBreakState;
+        private void ResetTmpVars()
+        {
+            tmpSpeed = tmpTemperature = tmpRpm = -1;
+            tmpBreakState = 'N'; // (N)ull
+        }
+        #endregion
 
         //private static RandomDataGenerator DataGenerator; // TODO TESTE
 
@@ -32,13 +61,16 @@ namespace TeleBajaUEA
             //return;// TODO TESTE
 
             // tempo para conectar com o carro
-            portXBee = new SerialPortBaja(Program.Settings.PortXBee);
+            portXBee = new SerialPortExt();
+            portXBee.PortName = Program.Settings.PortXBee;
+            portXBee.NewLine = XBEE_LINE_END.ToString();
+
             await Task.Run(() =>{ portXBee.Open(); });
 
             if (!portXBee.IsOpen)
                 throw new System.Exception("Porta não pôde ser aberta por razões desconhecidas.");
 
-            return await portXBee.TryHandshake();
+            return await TryHandshake();
         }
 
         public static void StartListen()
@@ -52,7 +84,7 @@ namespace TeleBajaUEA
             prevReceivedBytes = 0;
 
             // inicia leitura/salvamento dos dados recebidos pela porta USB
-            portXBee.StartReceiveData();
+            portXBee.WriteLine(XB_START);
             //DataGenerator.StartReceiveData();// TODO TESTE
         }
 
@@ -81,8 +113,38 @@ namespace TeleBajaUEA
 
         public async static Task<SensorsData> GetNextData()
         {
-            return await portXBee.GetNextPacket();
+            return await GetNextPacket();
             //return await DataGenerator.GetNextPacket();// TODO TESTE
+        }
+
+        // TODO implementar GetNextPacket (total de 10 bytes excluindo o 'B'
+        // pode gerar a exceção NextByteTimeoutException
+        // lê os dados, armazena em variáveis temporárias e gera um novo objeto SensorsData (newData)
+        public async static Task<SensorsData> GetNextPacket()
+        {
+            bool rcvOK = false;
+            string rcvMsg = await portXBee.ReadLineExt();
+
+            // flag BEGIN/END usadas para garantir que dados estarão sincronizados
+            if (rcvMsg.Equals(XB_BEGIN))
+            {
+                tmpMillis = await portXBee.NextUInt32();
+                tmpBreakState = await portXBee.NextChar();
+                tmpTemperature = await portXBee.NextInt16();
+                tmpRpm = await portXBee.NextInt16();
+                tmpSpeed = await portXBee.NextInt8();
+
+                rcvMsg = await portXBee.ReadLineExt();
+                if (rcvMsg.Equals(XB_END))
+                    rcvOK = true;
+            }
+
+            if (rcvOK)
+                return new SensorsData(tmpMillis, tmpSpeed, tmpTemperature,
+                                        tmpRpm, tmpBreakState);
+            else
+                throw new Exception("Protocolo incorreto. Esperava BEGIN('B'), " +
+                    "mas recebeu '" + rcvMsg);
         }
 
         public static bool IsPortAvaiable(string pPortName)
@@ -95,5 +157,74 @@ namespace TeleBajaUEA
 
             return false;
         }
+
+        // verifica estado da conexao do XBee e confirma se o carro está pronto
+        // para enviar dados
+        private async static Task<bool> TryHandshake()
+        {
+            string rcvMsg;
+            if (await ATConnectedToCar())
+            {
+                portXBee.WriteLine(XB_READY);
+
+                rcvMsg = await portXBee.ReadLineExt();
+
+                if (rcvMsg.Equals("OK"))
+                    return true;
+                else
+                {
+                    string errorMsg =
+                        "Protocolo incorreto (Handshake). Esperava 'OK', " +
+                        "mas recebeu '" + rcvMsg + "'";
+                    throw new Exception(errorMsg);
+                }
+            }
+            else
+            {
+                string errorMsg =
+                    "Sinal não encontrado, verifique o dispositivo no carro " +
+                    "e tente novamente";
+                throw new Exception(errorMsg);
+            }
+        }
+
+        #region funcoes de interface com o XBee
+        private async static Task<bool> ATConnectedToCar()
+        {
+            string rcv_msg = "";
+
+            // entrar no command mode
+            await ATcmd();
+
+            // verifica conexao
+            portXBee.WriteLine("ATDN" + XBEE_CAR_ID);
+            rcv_msg = await portXBee.ReadLineExt();
+
+            if (rcv_msg.Equals("OK"))
+                return true;
+            else if (rcv_msg.Equals("ERROR"))
+                return false;
+            else
+            {
+                string errorMsg =
+                    "Resposta do comando ATDN inesperada: \"" + rcv_msg + "\"";
+
+                throw (new Exception(errorMsg));
+            }
+        }
+
+        // TODO considerar acrescentar um timeout quando esperando pelo OK
+        private async static Task ATcmd()
+        {
+            await Task.Delay(XBEE_CMD_DELAY);
+            portXBee.Write("+++");
+
+            // quando entrar no modo de comando do XBee durante o recebimento de
+            // dados, é preciso descartar os dados que estão na fila até a chegada do OK
+            string readLine = await portXBee.ReadLineExt();
+            while (!readLine.Equals("OK"))
+                readLine = await portXBee.ReadLineExt();
+        }
+        #endregion
     }
 }
---
+++ src/TeleBajaUEA-client/TeleBajaUEA/ClassesAuxiliares/Extensões/SerialPortExt.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeleBajaUEA.RaceDataStructs;
+
+namespace TeleBajaUEA.ClassesAuxiliares
+{
+    class SerialPortExt : SerialPort
+    {
+        public uint TotalReceivedBytes { get { return byteCount; } }
+
+        // tempo em milisegundos até "NextByte()" lance uma exceção se não receber dados
+        private readonly double NEXT_BYTE_TIMEOUT = 15000;
+        private System.Timers.Timer timeoutRcvTimer;
+        private bool timeoutRcvExceeded;
+
+        #region Temporary received data buffer / Queue
+        private byte[] intBuffer = new byte[4];
+        private ConcurrentQueue<byte> receivedDataQueue = new ConcurrentQueue<byte>();
+        private uint byteCount = 0;
+        #endregion
+
+        public SerialPortExt() : base()
+        {
+            DataReceived += port_DataReceived;
+        }
+
+        #region eventos (event handlers) {...}
+        // acumula dados recebidos na porta serial em um buffer/fila para serem lidos depois
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] data = new byte[BytesToRead];
+            Read(data, 0, data.Length);
+            data.ToList().ForEach(b => { receivedDataQueue.Enqueue(b); byteCount++; } );
+        }
+
+        private void TimeoutTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            timeoutRcvExceeded = true;
+            StopTimeoutTimer();
+        }
+        #endregion
+
+        // lê linha a partir do buffer ConcurrentQueue
+        // considera um char como "NewLine", ao invés de uma string
+        public async Task<string> ReadLineExt()
+        {
+            string line = "";
+
+            char readChar = await NextChar();
+            while (readChar != NewLine[0])
+            {
+                line += readChar;
+                readChar = await NextChar();
+            }
+            return line;
+        }
+
+        // lê byte da ConcurrentQueue
+        public async Task<byte> NextByte()
+        {
+            StartTimeoutTimer();
+
+            byte rcvByte;
+            while (!receivedDataQueue.TryDequeue(out rcvByte))
+            {
+                if (timeoutRcvExceeded)
+                    throw new ErrorMessage.ReceiveDataTimeoutException();
+                else
+                    await Task.Delay(300);
+            }
+
+            // quando sair do while, sucesso, logo desativa o timer
+            StopTimeoutTimer();
+
+            return rcvByte;
+        }
+
+        public async Task<char> NextChar()
+        {
+            return (char) (await NextByte());
+        }
+
+        public async Task<uint> NextUInt32()
+        {
+            intBuffer[0] = intBuffer[1] = intBuffer[2] = intBuffer[3] = 0;
+
+            intBuffer[0] = await NextByte();
+            intBuffer[1] = await NextByte();
+            intBuffer[2] = await NextByte();
+            intBuffer[3] = await NextByte();
+
+            return BitConverter.ToUInt32(intBuffer, 0);
+        }
+
+        public async Task<int> NextInt16()
+        {
+            intBuffer[0] = intBuffer[1] = intBuffer[2] = intBuffer[3] = 0;
+
+            intBuffer[0] = await NextByte(); // ler o lowByte, a direita
+            intBuffer[1] = await NextByte(); // ler o highByte, a esquerda
+
+            return BitConverter.ToInt16(intBuffer, 0);
+        }
+
+        public async Task<int> NextInt8()
+        {
+            return await NextByte();
+        }
+
+        public void WriteChar(char c)
+        {
+            Write(c.ToString());
+        }
+
+        // TODO verificar se é melhor reutilizar um mesmo timer
+        // ou instanciar um novo sempre não impacta muito na performance
+        public void StartTimeoutTimer()
+        {
+            timeoutRcvExceeded = false;
+
+            timeoutRcvTimer = new System.Timers.Timer();
+            timeoutRcvTimer.Interval = NEXT_BYTE_TIMEOUT;
+            timeoutRcvTimer.Elapsed += TimeoutTimer_Elapsed;
+            timeoutRcvTimer.AutoReset = false;
+            timeoutRcvTimer.Enabled = true;
+            timeoutRcvTimer.Start();
+        }
+
+        private void StopTimeoutTimer()
+        {
+            if(timeoutRcvTimer != null)
+            {
+                timeoutRcvTimer.Enabled = false;
+                timeoutRcvTimer.Stop();
+                timeoutRcvTimer.Dispose();
+                timeoutRcvTimer = null;
+            }
+        }
+    }
+}
